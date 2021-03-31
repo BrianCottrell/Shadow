72bcb481dd73ceb945c0f4999a790e75cb50baa2
Chris Buonocore <chrisdistrict@gmail.com>
2021-03-31 00:20:10 -0400

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,81 @@
-const Config = require("./dfx.json");
-const Path = require("path");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const dfxJson = require("./dfx.json");
 
-// Identify build output directory.
-const output =
-  ["defaults", "build", "output"].reduce((accum, x) => {
-    return accum && accum[x] ? accum[x] : null;
-  }, Config) || Path.join(".dfx", "local", "canisters");
+// List of all aliases for canisters. This creates the module alias for
+// the `import ... from "ic:canisters/xyz"` where xyz is the name of a
+// canister.
+const aliases = Object.entries(dfxJson.canisters).reduce(
+  (acc, [name, _value]) => {
+    // Get the network name, or `local` by default.
+    const networkName = process.env["DFX_NETWORK"] || "local";
+    const outputRoot = path.join(
+      __dirname,
+      ".dfx",
+      networkName,
+      "canisters",
+      name
+    );
 
-// Identify canisters aliases.
-const aliases = Object.entries(Config.canisters).reduce((accum, [name]) => {
-  const outputRoot = Path.join(__dirname, output, name);
-  return {
-    ...accum,
-    ["ic:canisters/" + name]: Path.join(outputRoot, name + ".js"),
-    ["ic:idl/" + name]: Path.join(outputRoot, name + ".did.js"),
-  };
-}, {});
+    return {
+      ...acc,
+      ["ic:canisters/" + name]: path.join(outputRoot, name + ".js"),
+      ["ic:idl/" + name]: path.join(outputRoot, name + ".did.js"),
+    };
+  },
+  {}
+);
 
-// Generate webpack configuration.
-const generate = (name, info) => {
+/**
+ * Generate a webpack configuration for a canister.
+ */
+function generateWebpackConfigForCanister(name, info) {
   if (typeof info.frontend !== "object") {
     return;
   }
-  const inputRoot = __dirname;
-  const outputRoot = Path.join(__dirname, output, name);
+
   return {
-    entry: Path.join(inputRoot, info.frontend.entrypoint),
     mode: "production",
-    module: {
-      rules: [
-        {
-          loader: "ts-loader",
-          test: /\.(js|ts)x?$/,
-        },
-      ],
+    entry: {
+      index: path.join(__dirname, info.frontend.entrypoint),
     },
-    output: {
-      filename: "index.js",
-      path: Path.join(outputRoot, "assets"),
+    node: {
+      fs: "empty",
+    },
+    devtool: "source-map",
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
     },
     resolve: {
       alias: aliases,
     },
-  };
-};
+    output: {
+      filename: "[name].js",
+      path: path.join(__dirname, "dist", name),
+    },
 
+    // Depending in the language or framework you are using for
+    // front-end development, add module loaders to the default
+    // webpack configuration. For example, if you are using React
+    // modules and CSS as described in the "Adding a stylesheet"
+    // tutorial, uncomment the following lines:
+    module: {
+      rules: [
+        { test: /\.(ts|tsx|jsx)$/, loader: "ts-loader" },
+        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      ],
+    },
+    plugins: [],
+  };
+}
+
+// If you have additional webpack configurations you want to build
+//  as part of this configuration, add them to the section below.
 module.exports = [
-  ...Object.entries(Config.canisters)
+  ...Object.entries(dfxJson.canisters)
     .map(([name, info]) => {
-      return generate(name, info);
+      return generateWebpackConfigForCanister(name, info);
     })
     .filter((x) => !!x),
 ];
